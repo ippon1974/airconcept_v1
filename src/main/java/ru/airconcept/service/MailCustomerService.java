f5a32b0a7e7014642e9bb8c2708904bf3d0a2f7d
VOVAN <plategi@yandex.ru>
2022-02-02 20:48:53 +0300

    add policy from home

--- src/main/java/ru/airconcept/service/MailCustomerService.java
+++ src/main/java/ru/airconcept/service/MailCustomerService.java
@@ -51,7 +51,7 @@ public class MailCustomerService {
         session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("post2074@gmail.com", "k4b8c321974");
+                return new PasswordAuthentication("post2074@gmail.com", "55555");
             }
 
         });
@@ -154,7 +154,7 @@ public class MailCustomerService {
         session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("post2074@gmail.com", "000000");
+                return new PasswordAuthentication("post2074@gmail.com", "55555");
             }
 
         });
