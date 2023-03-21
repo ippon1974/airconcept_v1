6a671de187b2ddc066c3d34292f1d6b47147bb98
Vladimir <plategi@yandex.ru>
2023-03-21 17:29:40 +0300

    o

--- src/main/java/ru/airconcept/service/MailCustomerService.java
+++ src/main/java/ru/airconcept/service/MailCustomerService.java
@@ -52,7 +52,7 @@ public class MailCustomerService {
         session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("post2074@gmail.com", "offee");
+                return new PasswordAuthentication("post2074@gmail.com", "6546546546546546");
             }
 
         });
@@ -171,7 +171,7 @@ public class MailCustomerService {
         session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("post2074@gmail.com", "of");
+                return new PasswordAuthentication("post2074@gmail.com", "5465464654654");
             }
 
         });
